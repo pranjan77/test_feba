c835de16b2a003e62e9d75a50c33d08687757b54
Andrew Tritt <ajtritt@lbl.gov>
2013-10-29 11:42:06 -0700

    converting tabs to spaces for readability

--- bin/join.pl
+++ bin/join.pl
@@ -29,8 +29,8 @@ my @data2;
 my @iMatch1 = ();
 my @iMatch2 = ();
 
-my @bOut1 = ();			# true if output field is from 1
-my @iOut = ();			# index of each output field
+my @bOut1 = ();            # true if output field is from 1
+my @iOut = ();            # index of each output field
 my @bShow = ();
 
 my @newnames = ();
@@ -61,47 +61,47 @@ while(scalar @ARGV > 2) {
     my $option = shift @ARGV;
     my $value = shift @ARGV;
     if ($option eq "-header") {
-	$header = ($value eq "true" || $value eq "TRUE" || $value eq "yes" || $value eq "YES" || $value eq "1");
+    $header = ($value eq "true" || $value eq "TRUE" || $value eq "yes" || $value eq "YES" || $value eq "1");
     } elsif ($option eq "-match") {
-	foreach (split(/,/,$value)) {
-	    my @matches = split(/=/, $_);
-	    die $usage if scalar @matches != 2;
-	    push(@matchNames1, $matches[0]);
-	    push(@matchNames2, $matches[1]);
-	}
+    foreach (split(/,/,$value)) {
+        my @matches = split(/=/, $_);
+        die $usage if scalar @matches != 2;
+        push(@matchNames1, $matches[0]);
+        push(@matchNames2, $matches[1]);
+    }
     } elsif ($option eq "-rename") {
-	my @renames = split(/,/,$value);
-	foreach (@renames) {
-	    my @fields = split(/=/,$_,-1);	# no limit
-	    die $usage if scalar @fields != 2 || $fields[0] eq "";
-	    my $from = $fields[0];
-	    my $to = $fields[1];
-	    if ($from =~ m/^12\.(.*)$/) {
-		$fromBoth = $1;
-		my @tos = split(/\+/, $to);
-		die "Cannot parse dual rename $to" if scalar @tos != 2;
-		&addRename("1." . $fromBoth, $tos[0]);
-		&addRename("2." . $fromBoth, $tos[1]);
-	    } else {
-		&addRename($from,$to);
-	    }
-	}
+    my @renames = split(/,/,$value);
+    foreach (@renames) {
+        my @fields = split(/=/,$_,-1);    # no limit
+        die $usage if scalar @fields != 2 || $fields[0] eq "";
+        my $from = $fields[0];
+        my $to = $fields[1];
+        if ($from =~ m/^12\.(.*)$/) {
+        $fromBoth = $1;
+        my @tos = split(/\+/, $to);
+        die "Cannot parse dual rename $to" if scalar @tos != 2;
+        &addRename("1." . $fromBoth, $tos[0]);
+        &addRename("2." . $fromBoth, $tos[1]);
+        } else {
+        &addRename($from,$to);
+        }
+    }
     } elsif ($option eq "-keep") {
-	push(@keep, split(/,/,$value));
-	$keptOption = 1;
+    push(@keep, split(/,/,$value));
+    $keptOption = 1;
     } elsif ($option eq "-ignore") {
-	push(@ignore, split(/,/,$value) );
+    push(@ignore, split(/,/,$value) );
     } elsif ($option eq "-only") {
-	die $usage if $value != 1 && $value != 2 && $value != 12 && $value != 10 && $value != 120;
-	$only = $value;
+    die $usage if $value != 1 && $value != 2 && $value != 12 && $value != 10 && $value != 120;
+    $only = $value;
     } elsif ($option eq "-case") {
-	$case = ($value eq "true" || $value eq "TRUE" || $value eq "yes" || $value eq "YES" || $value eq "1");
+    $case = ($value eq "true" || $value eq "TRUE" || $value eq "yes" || $value eq "YES" || $value eq "1");
     } elsif ($option eq "-uniq") {
-	$uniq = ($value eq "true" || $value eq "TRUE" || $value eq "yes" || $value eq "YES" || $value eq "1");
+    $uniq = ($value eq "true" || $value eq "TRUE" || $value eq "yes" || $value eq "YES" || $value eq "1");
     } elsif ($option eq "-scan") {
-	$scan = ($value eq "true" || $value eq "TRUE" || $value eq "yes" || $value eq "YES" || $value eq "1");
+    $scan = ($value eq "true" || $value eq "TRUE" || $value eq "yes" || $value eq "YES" || $value eq "1");
     } else {
-	die "Cannot recognize option $option:\n$usage";
+    die "Cannot recognize option $option:\n$usage";
     }
 }
 
@@ -122,23 +122,23 @@ die $usage if $file1 eq $file2 && $file1 eq "-";
 sub load($$$$) {
     my ($file, $nameref, $dataref,$scan) = @_;
     if ($file eq "-") {
-	open(FILE, "<&STDIN") || die "Cannot dup STDIN";
+    open(FILE, "<&STDIN") || die "Cannot dup STDIN";
     } else {
-	open(FILE, "<", $file) || die "Cannot read $file";
+    open(FILE, "<", $file) || die "Cannot read $file";
     }
     my $first = 1;
     while(<FILE>) {
-	$_ =~ s/[\r\n]+$//;
-	my @F = split(/\t/, $_, -1);
-	if ($first) {
-	    push( @{ $nameref }, @F ); # save first line as names even if no header for length checking
-	}
-	if ($header ? !$first : 1) {
-	    push( @{ $dataref }, \@F );	# first line is data if no header
-	}
-	die "Too few columns in $file" if (!$first && scalar @F < scalar @{ $nameref });
-	$first = 0;
-	return if $scan;
+    $_ =~ s/[\r\n]+$//;
+    my @F = split(/\t/, $_, -1);
+    if ($first) {
+        push( @{ $nameref }, @F ); # save first line as names even if no header for length checking
+    }
+    if ($header ? !$first : 1) {
+        push( @{ $dataref }, \@F );    # first line is data if no header
+    }
+    die "Too few columns in $file" if (!$first && scalar @F < scalar @{ $nameref });
+    $first = 0;
+    return if $scan;
     }
     close(FILE) || die "Cannot close $file";
 }
@@ -146,8 +146,8 @@ sub load($$$$) {
 sub getColumn($$$) {
     my ($name, $prefix, $namesref) = @_;
     if ($name =~ m/^(.*)\.(.*)$/) {
-	return -1 if $1 ne $prefix;
-	$name = $2;
+    return -1 if $1 ne $prefix;
+    $name = $2;
     }
     return $name - 1 if ($name =~ m/^[0-9]+/ && $name > 0 && $name <= scalar @{$namesref} );
     return -1 if !$header;
@@ -175,38 +175,38 @@ sub printMatches($$) {
     my @F1s = @{ $list1 };
     my @F2s = @{ $list2 };
     foreach (@F1s) {
-	my @F1 = @{ $_ };
-	my $nDups = ($uniq ? (scalar @F1s)*(scalar @F2s) : 1);
-	foreach (@F2s) {
-	    my @F2 = @{ $_ };
-	    my $first = 1;
-	    # Finally we get to print
-	    foreach (0..$#bShow) {
-		if ($bShow[$_]) {
-		    my $b1 = $bOut1[$_];
-		    my $i = $iOut[$_];
-		    my $field;
-		    if ($b1 && scalar @F1 == 0) {
-			$field = "";
-		    } elsif (!$b1 && scalar @F2 == 0) {
-			$field = "";
-		    } elsif ($b1) {
-			$field = $F1[$i];
-		    } else {
-			$field = $F2[$i];
-		    }
-		    if ($nDups > 1) {
-			print STDERR "\t" if (!$first);
-			print STDERR $field;
-		    } else {
-			print "\t" if (!$first);
-			print $field;
-		    }
-		    $first = 0;
-		}
-	    }
-	    if ($nDups > 1) { print STDERR "\t$nDups\n"; }  else { print "\n"; }
-	}
+    my @F1 = @{ $_ };
+    my $nDups = ($uniq ? (scalar @F1s)*(scalar @F2s) : 1);
+    foreach (@F2s) {
+        my @F2 = @{ $_ };
+        my $first = 1;
+        # Finally we get to print
+        foreach (0..$#bShow) {
+        if ($bShow[$_]) {
+            my $b1 = $bOut1[$_];
+            my $i = $iOut[$_];
+            my $field;
+            if ($b1 && scalar @F1 == 0) {
+            $field = "";
+            } elsif (!$b1 && scalar @F2 == 0) {
+            $field = "";
+            } elsif ($b1) {
+            $field = $F1[$i];
+            } else {
+            $field = $F2[$i];
+            }
+            if ($nDups > 1) {
+            print STDERR "\t" if (!$first);
+            print STDERR $field;
+            } else {
+            print "\t" if (!$first);
+            print $field;
+            }
+            $first = 0;
+        }
+        }
+        if ($nDups > 1) { print STDERR "\t$nDups\n"; }  else { print "\n"; }
+    }
     }
 }
 
@@ -214,30 +214,30 @@ sub printMatches($$) {
 &load($file1,\@names1,\@data1,$scan); # FILE still open if $scan
 
 @iMatch1 = map { my $i = &getColumn($_, 1, \@names1);
-		 die "Cannot find match name for $_" if $i < 0;
-		 $i;
-	     } @matchNames1;
+         die "Cannot find match name for $_" if $i < 0;
+         $i;
+         } @matchNames1;
 push(@iMatch1, 0) if scalar @iMatch1 == 0;
 
 @iMatch2 = map { my $i = &getColumn($_, 2, \@names2);
-		 die "Cannot find match name for $_" if $i < 0;
-		 $i;
-	     } @matchNames2;
+         die "Cannot find match name for $_" if $i < 0;
+         $i;
+         } @matchNames2;
 push(@iMatch2, 0) if scalar @iMatch2 == 0;
 
 
 if (scalar @keep > 0) {
     foreach my $out (@keep) {
-	my $i = &getColumn($out, 1, \@names1);
-	my $b1 = 1;
-	if ($i == -1) {
-	    $i = &getColumn($out, 2, \@names2);
-	    $b1 = 0;
-	}
-	die "Cannot recognize keep $out" if $i < 0;
-	push(@bOut1, $b1);
-	push(@iOut, $i);
-	push(@bShow,1);
+    my $i = &getColumn($out, 1, \@names1);
+    my $b1 = 1;
+    if ($i == -1) {
+        $i = &getColumn($out, 2, \@names2);
+        $b1 = 0;
+    }
+    die "Cannot recognize keep $out" if $i < 0;
+    push(@bOut1, $b1);
+    push(@iOut, $i);
+    push(@bShow,1);
     }
 } else {
     map { push(@bOut1, 1); push(@iOut, $_); } (0..$#names1);
@@ -245,31 +245,31 @@ if (scalar @keep > 0) {
     map { push(@bShow, 1); } (0..$#iOut);
 
     foreach my $ign (@ignore) {
-	my $i = &getColumn($ign, 1, \@names1);
-	my $b1 = 1;
-	if ($i == -1) {
-	    $i = &getColumn($ign, 2, \@names2);
-	    $b1 = 0;
-	}
-	die "Cannot recognize ignore $ign" if $i < 0;
-	$i += scalar @names1 if !$b1;
-	$bShow[$i] = 0;
+    my $i = &getColumn($ign, 1, \@names1);
+    my $b1 = 1;
+    if ($i == -1) {
+        $i = &getColumn($ign, 2, \@names2);
+        $b1 = 0;
+    }
+    die "Cannot recognize ignore $ign" if $i < 0;
+    $i += scalar @names1 if !$b1;
+    $bShow[$i] = 0;
     }
 }
 
 if ($header) {
     foreach (0..$#iOut) {
-	my $i = $iOut[$_];
-	my $b1 = $bOut1[$_];
-	my $oldname = $b1 ? $names1[$i] : $names2[$i];
-	my $newname = $oldname;
-	my $prefix = $b1 ? "1" : "2";
-	if (exists $rename{$oldname}) {
-	    $newname = $rename{$oldname};
-	} elsif (exists $rename{"$prefix.$oldname"}) {
-	    $newname = $rename{"$prefix.$oldname"};
-	}
-	push(@newnames, $newname);
+    my $i = $iOut[$_];
+    my $b1 = $bOut1[$_];
+    my $oldname = $b1 ? $names1[$i] : $names2[$i];
+    my $newname = $oldname;
+    my $prefix = $b1 ? "1" : "2";
+    if (exists $rename{$oldname}) {
+        $newname = $rename{$oldname};
+    } elsif (exists $rename{"$prefix.$oldname"}) {
+        $newname = $rename{"$prefix.$oldname"};
+    }
+    push(@newnames, $newname);
     }
 }
 
@@ -287,15 +287,15 @@ if ($only == -1 || $only == 1 || $only == 12 || $only == 10 || $only == 120) {
 if ($header) {
     my $first = 1;
     foreach (0..$#newnames) {
-	if ($bShow[$_]) {
-	    print "\t" if (!$first);
-	    print $newnames[$_];
-	    if ($uniq) {
-		print STDERR "\t" if (!$first);
-		print STDERR $newnames[$_];
-	    }
-	    $first = 0;
-	}
+    if ($bShow[$_]) {
+        print "\t" if (!$first);
+        print $newnames[$_];
+        if ($uniq) {
+        print STDERR "\t" if (!$first);
+        print STDERR $newnames[$_];
+        }
+        $first = 0;
+    }
     }
     print "\n";
     print STDERR "\tnDuplicates\n" if $uniq;
@@ -303,49 +303,49 @@ if ($header) {
 
 if ($only == -1) {
     if ($scan) {
-	while(<FILE>) {
-	    $_ =~ s/[\r\n]+$//;
-	    my @F = split(/\t/, $_, -1);
-	    die "Too few columns in $file1" if scalar @F < scalar @names1;
-	    my @F1s = ( \@F );
-	    my $match = joinFields(\@F, \@iMatch1);
-	    &printMatches(\@F1s, $index2{myuc($match)}) if exists $index2{myuc($match)};
-	}
-	close (FILE) || die "Cannot close $file1";
+    while(<FILE>) {
+        $_ =~ s/[\r\n]+$//;
+        my @F = split(/\t/, $_, -1);
+        die "Too few columns in $file1" if scalar @F < scalar @names1;
+        my @F1s = ( \@F );
+        my $match = joinFields(\@F, \@iMatch1);
+        &printMatches(\@F1s, $index2{myuc($match)}) if exists $index2{myuc($match)};
+    }
+    close (FILE) || die "Cannot close $file1";
     } else {
-	foreach (@data1) {
-	    my @F1 = @{ $_ };
-	    my @F1s = ( \@F1 );
-	    my $match = joinFields(\@F1, \@iMatch1);
-	    &printMatches(\@F1s, $index2{myuc($match)}) if exists $index2{myuc($match)};
-	}
+    foreach (@data1) {
+        my @F1 = @{ $_ };
+        my @F1s = ( \@F1 );
+        my $match = joinFields(\@F1, \@iMatch1);
+        &printMatches(\@F1s, $index2{myuc($match)}) if exists $index2{myuc($match)};
+    }
     }
 }
 
 if($only == 1 || $only == 12) {
     foreach (@data1) {
-	my @F1 = @{ $_ };
-	my @F1s = ( \@F1 );
-	my $match = joinFields(\@F1, \@iMatch1);
-	&printMatches(\@F1s, \@emptyempty) if !exists $index2{myuc($match)};
+    my @F1 = @{ $_ };
+    my @F1s = ( \@F1 );
+    my $match = joinFields(\@F1, \@iMatch1);
+    &printMatches(\@F1s, \@emptyempty) if !exists $index2{myuc($match)};
     }
 }
 
 if ($only == 10 || $only == 120) {
     foreach (@data1) {
-	my @F1 = @{ $_ };
-	my @F1s = ( \@F1 );
-	my $match = joinFields(\@F1, \@iMatch1);
-	&printMatches(\@F1s, (exists $index2{myuc($match)} ? $index2{myuc($match)} : \@emptyempty));
+    my @F1 = @{ $_ };
+    my @F1s = ( \@F1 );
+    my $match = joinFields(\@F1, \@iMatch1);
+    &printMatches(\@F1s, (exists $index2{myuc($match)} ? $index2{myuc($match)} : \@emptyempty));
     }
 }
 
 if ($only == 2 || $only == 12 || $only == 120) {
     foreach (@data2) {
-	my @F2 = @{ $_ };
-	my @F2s = ( \@F2 );
-	my $match = joinFields(\@F2, \@iMatch2);
-	&printMatches(\@emptyempty, \@F2s) if !exists $index1{myuc($match)};
+    my @F2 = @{ $_ };
+    my @F2s = ( \@F2 );
+    my $match = joinFields(\@F2, \@iMatch2);
+    &printMatches(\@emptyempty, \@F2s) if !exists $index1{myuc($match)};
     }
 }
 
--- bin/sumCols.pl
+++ bin/sumCols.pl
@@ -9,16 +9,16 @@ use strict;
     my @n = map { 0 } @colnames;
     my $nLines = 0;
     while (my $line = <STDIN>) {
-	chomp $line;
-	my @F = split /\t/, $line;
-	foreach my $i (0 .. (scalar(@F)-1)) {
-	    $n[$i] += $F[$i] if $F[$i] =~ m/^-?[0-9]+[0-9.]*$/;
-	}
-	$nLines++;
+    chomp $line;
+    my @F = split /\t/, $line;
+    foreach my $i (0 .. (scalar(@F)-1)) {
+        $n[$i] += $F[$i] if $F[$i] =~ m/^-?[0-9]+[0-9.]*$/;
+    }
+    $nLines++;
     }
     print STDERR "Read $nLines data lines\n";
     if ($nLines > 0) {
-	print join("\t", @colnames)."\n";
-	print join("\t", @n)."\n";
+    print join("\t", @colnames)."\n";
+    print join("\t", @n)."\n";
     }
 }
