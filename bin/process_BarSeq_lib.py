81f902fbc3b1e9a9c11f343d2d335f9b70acd318
Andrew Tritt <ajtritt@lbl.gov>
2013-12-17 10:36:18 -0800

    got process_BarSeq_lib.py to work with output from abhi's output.

--- bin/process_BarSeq_lib.py
+++ bin/process_BarSeq_lib.py
@@ -8,6 +8,7 @@ import collections
 import copy
 import Queue
 import string
+import datetime
 
 ##updating the library look up path
 sys.path.append(os.path.join(os.path.dirname(__file__),'../lib'));
@@ -22,6 +23,7 @@ QUEUE_SIZE = 100000
 def read_reads(read_files, queue, buffer_size=1000, verbose=False):
     proc_name = multiprocessing.current_process().name
     count = 0
+    start = datetime.datetime.now()
     buf = [None]*buffer_size
     added = False
     for read_file in read_files:
@@ -35,17 +37,20 @@ def read_reads(read_files, queue, buffer_size=1000, verbose=False):
                         for item in buf:
                             queue.put(item)
                         #map(queue.put,buf)
+                        end = datetime.datetime.now()
                         if verbose:
-                            sys.stderr.write("[%s] added %d reads to the input queue\n" % (proc_name,count))
+                            sys.stderr.write("[%s] added %d reads to the input queue. Took %s to complete\n" % (proc_name,count, str(end-start)))
                         added = True
                 count = 0   
+                start = datetime.datetime.now()
     added = False
     while not added:
         if queue.qsize() < QUEUE_SIZE - count:
             for i in xrange(count):
                 queue.put(buf[i])
+            end = datetime.datetime.now()
             if verbose:
-                sys.stderr.write("[%s] added remaining %d reads to the input queue\n" % (proc_name,count))
+                sys.stderr.write("[%s] added remaining %d reads to the input queue. Took %s to complete\n" % (proc_name,count, str(end-start)))
             added = True
         if verbose:
             sys.stderr.write("[%s] sending STOP signal\n" % (proc_name))
@@ -63,6 +68,7 @@ def process_reads(parser, in_queue, out_queue, buffer_size=1000, verbose=False):
     barcodes = [None]*buffer_size
     more_reads = True
     bc_count = 0
+    start = datetime.datetime.now()
     mplex_bc, barseq_bc = None, None
     retrieved = False
     while more_reads:
@@ -72,7 +78,7 @@ def process_reads(parser, in_queue, out_queue, buffer_size=1000, verbose=False):
             if not in_queue.empty():
                 i = 0
                 for i in xrange(buffer_size):
-                    reads[i] = in_queue.get(timeout=3)
+                    reads[i] = in_queue.get()
                     if reads[i] == id(STOP):
                         if verbose:
                             sys.stderr.write("[%s] found STOP signal\n" % (proc_name))
@@ -95,15 +101,17 @@ def process_reads(parser, in_queue, out_queue, buffer_size=1000, verbose=False):
             if bc_count > 0 and (bc_count == buffer_size or read == id(STOP)):
                 for i in xrange(bc_count):
                     barcode_counts[barcodes[i][0]][barcodes[i][1]] += 1
+                end = datetime.datetime.now()
                 if verbose:
-                    sys.stderr.write("[%s] counted %d barcodes\n" % (proc_name,bc_count))
+                    sys.stderr.write("[%s] counted %d barcodes. Took %s to complete\n" % (proc_name,bc_count, str(end-start)))
                 bc_count = 0
+                start = datetime.datetime.now()
             if not more_reads:
                 break
     out_queue.put(barcode_counts)
     if verbose:
         #sys.stderr.write("[%s] added counts to out queue\n" % (proc_name))
-        sys.stderr.write("[%s] added counts to out queue. %d total items in the queue, id=%d\n" % (proc_name, out_queue.qsize(), id(out_queue)))
+        sys.stderr.write("[%s] added counts to out queue. \n" % (proc_name, out_queue.qsize(), id(out_queue)))
 
     return 0
 #end process_reads
@@ -126,6 +134,7 @@ def get_usr_args():
     parser.add_argument("fastq", nargs='+', type=str, help="One or more fastqs to count barcodes for")
     parser.add_argument("-eN","--expName", type=str, dest="exp_name", default=None, metavar="", help="Name of the experiment")
     parser.add_argument("-p", "--procs", type=int, dest="procs", default=multiprocessing.cpu_count(), help="the number of processes to use")
+    parser.add_argument("-b", "--buffer_size", type=int, dest="buffer_size", default=10000, help="the size of the buffer for processing reads")
     parser.add_argument("-v", "--verbose", action="store_true", default=False, help="be verbose")
     
     if len(sys.argv) == 1:
@@ -188,16 +197,22 @@ def main():
     # a Queue for getting passing counts back to the master process
     out_queue = multiprocessing.Queue()
     
-    procs[0] = multiprocessing.Process(target=read_reads, args=(args.fastq,in_queue,10000,args.verbose)) 
-    procs[1] = multiprocessing.Process(target=process_reads, args=(copy.deepcopy(read_parser),in_queue,out_queue,10000,args.verbose))
+    reader_buffer_size = args.buffer_size 
+    process_buffer_size = reader_buffer_size
+
+    procs[0] = multiprocessing.Process(target=read_reads, args=(args.fastq,in_queue,reader_buffer_size,args.verbose)) 
+    procs[1] = multiprocessing.Process(target=process_reads, args=(copy.deepcopy(read_parser),in_queue,out_queue,process_buffer_size,args.verbose))
     for i in xrange(2,len(procs)):
-        procs[i] = multiprocessing.Process(target=process_reads, args=(copy.deepcopy(read_parser),in_queue,out_queue,10000,args.verbose))
+        procs[i] = multiprocessing.Process(target=process_reads, args=(copy.deepcopy(read_parser),in_queue,out_queue,process_buffer_size,args.verbose))
 
 
 
     # start and wait for our processes to finish
     #for p in procs:
     #for i in xrange(0,len(procs),-1):
+   
+    start = datetime.datetime.now()
+    sys.stderr.write("[%s] %s\n" % (proc_name,start.strftime("DATE: %02m/%02d/%04Y %02H:%0M:%02S")))
     for i in xrange(len(procs)):
         sys.stderr.write("[%s] Attempting to start process %s\n" % (proc_name,procs[i].name))
         procs[i].start()    
@@ -213,7 +228,8 @@ def main():
     sys.stderr.write("[%s] Read all counts\n" % (proc_name))
     
     sys.stderr.write("[%s] Loading pool file\n" % (proc_name))
-    barseq_barcodes = read_morgan_pool_file(args.pool_file)
+    #barseq_barcodes = read_morgan_pool_file(args.pool_file)
+    barseq_barcodes = read_pool_file(args.pool_file)
     sys.stderr.write("[%s] Found %d barcodes in pool file\n" % (proc_name,len(barseq_barcodes)))
     #barseq_barcodes = read_pool_file(args.pool_file)
     out = sys.stdout
@@ -222,23 +238,32 @@ def main():
     out.write("barcode\trcbarcode\tscaffold\tstrand\tpos")
     for mplex_bc in mplex_barcodes.keys():
         out.write("\t%s" % mplex_barcodes[mplex_bc].name)
+    out.write("\n")
     for barseq_bc in barseq_barcodes.keys():
         tn_seq, tn_strand, tn_pos = barseq_barcodes[barseq_bc] 
-        out.write("%s\t%s\t%s\t%s\t%s" % (barseq_bc,rev_comp(barseq_bc),tn_seq,tn_strand,tn_pos))
+        rc_bc = rev_comp(barseq_bc)
+        out.write("%s\t%s\t%s\t%s\t%s" % (barseq_bc,rc_bc,tn_seq,tn_strand,tn_pos))
         for mplex_bc in mplex_barcodes.keys():
-            if barseq_bc in counts[mplex_bc]:
-                out.write("\t%d" % counts[mplex_bc][barseq_bc])
+            #if barseq_bc in counts[mplex_bc]:
+            if rc_bc in counts[mplex_bc]:
+                sys.stderr.write("Found %s in table\n" % rc_bc)
+                out.write("\t%d" % counts[mplex_bc][rc_bc])
             else:
                 out.write("\t0")
         out.write("\n")
     
+    end = datetime.datetime.now()
+    sys.stderr.write("[%s] %s\n" % (proc_name,end.strftime("DATE: %02m/%02d/%04Y %02H:%0M:%02S")))
+    
+    sys.stderr.write("[%s] Took %s to complete\n" % (proc_name,str(end-start)))
 
+    sys.exit(0)
     # export counts to stdout 
     for mplex_bc in counts.keys():
         print mplex_barcodes[mplex_bc].name
         for barseq_bc in counts[mplex_bc]:
             print "%s\t%s\t%d" % (mplex_bc,barseq_bc,counts[mplex_bc][barseq_bc]) 
-    
+
     
     
 #end main
