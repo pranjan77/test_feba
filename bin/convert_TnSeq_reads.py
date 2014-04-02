92a53a7fd0a19e6bd80e820228b6a4824ee6bbd4
Andrew Tritt <ajtritt@lbl.gov>
2014-04-02 16:01:53 -0700

    added default model to convert_TnSeq_reads.py

--- bin/convert_TnSeq_reads.py
+++ bin/convert_TnSeq_reads.py
@@ -6,11 +6,29 @@ import argparse
 import feba
 import readSeq
 
+'''
+Function to ensure the read model is valid input
+'''
+def __read_model__ (string):
+    model = None
+    if os.path.isfile(string):
+        with open(read_model, 'r') as read_model_file:
+            read_model_file.readline()
+            model = read_model_file.readline()[:-1]
+    else:
+        model = string
+    if len(re.sub('[nNATCG]', "", model)) > 0:
+        raise argparse.ArgumentTypeError("%s is not a valid model.\nMake sure it consists of only nNATCG")
+    return model
+#end __read_model__
+        
+__DEFAULT_MODEL__ = "nnnnnnCGCCCTGCAGGGATGTCCACGAGGTCTCTNNNNNNNNNNNNNNNNNNNNCGTACGCTGCAGGTCGACGGCCGGCCAGACCGGGGACTTATCAGCCAACCTGT"
+
 def get_args():
     desc = "Process TnSeq reads by removing trimming pre and post sequences and barcode. Reads printed to stdout"
-    parser = argparse.ArgumentParser(usage="%(prog)s model fastq [options]", description=desc)
-    parser.add_argument("model", type=str, help="the moduel with which to process reads")
+    parser = argparse.ArgumentParser(usage="%(prog)s fastq [options]", description=desc)
     parser.add_argument("fastq", type=str, help="the fastq to process")
+    parser.add_argument("-m", "--model", type=str, help="the model with which to process reads")
     parser.add_argument("-u","--unpaired", action='store_true', default=False, help="reads are single-end")
     
     return parser.parse_args()
