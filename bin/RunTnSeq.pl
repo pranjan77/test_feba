b51956b21c0e307efea48563464c50919e4274cb
Andrew Tritt <ajtritt@lbl.gov>
2014-04-23 00:27:40 -0700

    added support for switching genepool shares and suport for multiple fastqs to RunTnSeq.pl

--- bin/RunTnSeq.pl
+++ bin/RunTnSeq.pl
@@ -9,14 +9,25 @@ use FEBA_utils;
 {
     my $debug = 0;
     my $num_lines = 50*1000*1000;
-    
+    my $gp_project = "gentech-rqc";
+    my $help = 0;
     GetOptions('debug'       => \$debug,
+               'project=s'   => \$gp_project,
+               'help|h'      => \$help,
                'lines|l=i'   => \$num_lines);
-    die "Usage: RunTnSeq.pl ref.fa model_file fastq.gz outdir outbase\n"
-        . "   i.e., RunTnSeq.pl Phaeo Phaeo_ML1 pKMW7 fastafile\n"
-        unless @ARGV==5;
-    my ($fna_file, $model_file, $fastq, $outdir, $outbase) = @ARGV;
-    die "No such file: $fastq" unless -e $fastq;
+    die "Usage: RunTnSeq.pl [--project project] ref.fa model_file outdir outbase fastq1.gz fastq2.gz ... fastqN.gz\n"
+        . "   i.e., RunTnSeq.pl Phaeo Phaeo_ML1 pKMW7 fastafile reads1.fastq.gz reads2.fastq.gz reads3.fastq.gz\n"
+        if $help or @ARGV < 5;
+    my $fna_file = shift;
+    my $model_file = shift;
+    my $outdir = shift; 
+    my $outbase = shift;
+
+    my $fastq_files = join(" ",@ARGV);
+    
+    for my $fastq (@ARGV){
+        die "No such file: $fastq" unless -e $fastq;
+    }
     
     mkdir $outdir unless -d $outdir;
     mkdir "$outdir/cmds" unless -d "$outdir/cmds";
@@ -24,8 +35,8 @@ use FEBA_utils;
     
     die "No model file: $model_file" unless -e $model_file;
 
-    print STDERR "gunzipping and splitting $fastq\n";
-    system("gunzip -c $fastq | split -l $num_lines -d - $outdir/${outbase}_TnSeq.part") == 0 || die "Split failed";
+    print STDERR "gunzipping and splitting $fastq_files\n";
+    system("gunzip -c $fastq_files | split -l $num_lines -d - $outdir/${outbase}_TnSeq.part") == 0 || die "Split failed";
     
     my $part_glob = "$outdir/${outbase}_TnSeq.part[0-9][0-9]";
     my @fq_parts = glob $part_glob; 
@@ -47,7 +58,7 @@ use FEBA_utils;
         open (my $qsub_script_fh,">",$qsub_script) or die "Can't open $qsub_script\n";
         my @modules = ( "blat" );
         my @cmds = ( $map_tn_seq_cmd );
-        FEBA_utils::create_job_array_script($qsub_script_fh,1,"5G","12:00:00",\@cmds, \@modules);
+        FEBA_utils::create_job_array_script($qsub_script_fh,1,"5G","12:00:00", $gp_project, \@cmds, \@modules );
         close $qsub_script_fh;
         
         my $job_id = FEBA_utils::submit_job_array($qsub_script,"runTnSeq",1,scalar(@fq_parts),1,$debug);
