5e41b303bc0b3bb7b65390ca319cdd6d4c141001
Morgan Price <funwithwords26@gmail.com>
2014-09-24 16:19:12 -0700

    RunTnSeqLocal.pl can handle one giant fastq.gz file by splitting it

--- bin/RunTnSeqLocal.pl
+++ bin/RunTnSeqLocal.pl
@@ -5,8 +5,8 @@ use FindBin qw($Bin);
 use File::Which;
 
 my $usage = <<END
-Usage: RunTnSeqLocal.pl [ -limit nMaxReadsPerFile ] [ -blat blatcmd ]
-    nickname library_name modelsuffix fastq_directory
+Usage: RunTnSeqLocal.pl [ -nosplit ] [ -limit nMaxReadsPerFile ] [ -blat blatcmd ]
+    nickname library_name modelsuffix fastq_directory_or_gz
 Must be run in the parent directory of g/nickname
 The file model_modelsuffix must exist in the feba primers/ directory.
 Writes to g/nickname/library_name
@@ -17,27 +17,64 @@ END
 {
     my $limit = undef;
     my $blat = -e "$Bin/blat" ? "$Bin/blat" : "blat";
+    my $nosplit = undef;
     die $usage unless GetOptions('limit=i' => \$limit,
-				 'blat=s' => \$blat)
+				 'blat=s' => \$blat,
+	                         'nosplit' => \$nosplit)
 	&& @ARGV==4;
     my ($nickname, $library, $modelsuffix, $fastqdir) = @ARGV;
 
     die "blat is not on the path!" if ($blat eq "blat" && !defined which("blat"));
 
-    die "Not a directory: $fastqdir" unless -d $fastqdir;
     die "No such file: g/$nickname" unless -d "g/$nickname";
     my $modelfile = "$Bin/../primers/model_".$modelsuffix;
     die "No such file: $modelfile" unless -e $modelfile;
 
+    die "Not such file: $fastqdir" unless -e $fastqdir;
+    if (! -d $fastqdir) {
+	# split the gz file into pieces
+	my $file = $fastqdir;
+	my $gzipped = 0;
+	if ($file =~ m/[.]gz$/) {
+	    $gzipped = 1;
+	}
+	$fastqdir =~ s/fastq[.]gz$//;
+	$fastqdir =~ s/fastq$//;
+	$fastqdir .= ".parts";
+	if (! -d $fastqdir) {
+	    mkdir($fastqdir) || die "Cannot mkdir $fastqdir: $!";
+	    undef $nosplit;
+	}
+	my $zcat_cmd = $gzipped ? "zcat $file" : "cat $file";
+	my $num_lines = 20*1000*1000;
+	if (defined $nosplit ) {
+	    print STDERR "Skipping splitting $file into pieces in $fastqdir\n";
+	} else {
+	    print STDERR "Splitting $file into pieces in $fastqdir\n";
+	    system("$zcat_cmd | split --lines=$num_lines --numeric-suffixes - $fastqdir/TnSeq_part.fastq") == 0
+		|| die "split failed";
+	}
+    }
     my $cmdsfile = "$fastqdir/RunTnSeqLocal.cmds";
     system("rm ${cmdsfile}*") if -e $cmdsfile;
 
-    open(CMDS, ">", $cmdsfile) || die "Error writing to $cmdsfile";
-    my @inputs = glob("$fastqdir/*.fastq.gz");
+    my @inputs = ();
+    @inputs = glob("$fastqdir/*.fastq.gz");
+    @inputs = glob("$fastqdir/*.fastq") if (scalar(@inputs) == 0);
+    @inputs = glob("$fastqdir/TnSeq_part.fastq*[0-9]") if (scalar(@inputs) == 0);
+    die "Cannot find input fastq or fastq.gz files in $fastqdir" if scalar(@inputs) == 0;
     my @mapped = ();
+
+    open(CMDS, ">", $cmdsfile) || die "Error writing to $cmdsfile";
     foreach my $file (@inputs) {
 	my $base = $file;
-	$base =~ s/.fastq.gz$//;
+	if ($file =~ m/[.]fastq[.]gz/) {
+	    $base =~ s/[.]fastq[.]gz$//;
+	} elsif ($file =~ m/[.]fastq[.]/) {
+	    $base =~ s/[.]fastq//;
+	} elsif ($file =~ m/part[.]fastq\d+/) {
+	    $base =~ s/part[.]fastq/part/;
+	}
 	push @mapped, "$base.mapped";
 	my $cmd = "$Bin/MapTnSeq.pl -genome g/$nickname/genome.fna -model $modelfile -first $file";
 	$cmd .= " -limit $limit" if defined $limit;
@@ -47,11 +84,12 @@ END
 	print CMDS "$cmd\n";
     }
     close(CMDS) || die "Error writing to $cmdsfile";
-    print STDERR "Wrote " . scalar(@inputs) . " mapping jobs to $cmdsfile";
+    print STDERR "Wrote " . scalar(@inputs) . " mapping jobs to $cmdsfile\n";
     system("$Bin/submitter.pl",$cmdsfile) == 0
 	|| die "Error running $Bin/submitter.pl on $cmdsfile\n";
 
     system("(grep -h Reads $cmdsfile-*.log; grep -h Proportion $cmdsfile-*.log) >& g/$nickname/$library.reads");
+    print STDERR "Wrote g/$nickname/$library.reads\n";
 
     my $cmd = "$Bin/DesignRandomPool.pl -minN 10 -genes g/$nickname/genes.tab -pool g/$nickname/$library.pool "
 	. join(" ",@mapped) . " >& g/$nickname/$library.pool.stats";
