75018f67d8eb6345575d8fb0b9246769c417f21c
Morgan Price <funwithwords26@gmail.com>
2015-09-01 21:49:32 -0700

    Minor rendering improvements

--- images/fitblast.js
+++ images/fitblast.js
@@ -1,11 +1,9 @@
 /* fitblast.js -- javascript code for fitness BLAST
 
    Fill an HTML element with a short summary of the fitness BLAST results:
-
    fitblast_load_short(element_id, server_root, sequence);
 
    Or fill an HTML element with a table of fitness BLAST results: 
-
    fitblast_load_table(element_id, server_root, sequence);
 
    See fitblast_example.html
@@ -48,7 +46,7 @@ function fitblast_short(row,server_root) {
     var hascofit = row.maxcofit >= mincofit;
     var hassig = (row.minfit < -minabsfit && row.minT < -minabsT) || (row.maxfit > minabsfit && row.maxT > minabsT);
     var hasstrong = (row.minfit < -minabsstrong && row.minT < -minabsT) || (row.maxfit > minabsstrong && row.maxT > minabsT);
-    var sigstring = hasstrong ? "strong phenotype" : (hassig? "phenotype" : (row.minfit !== "" ? "has data" : "no data"));
+    var sigstring = hasstrong ? "strong phenotype" : (hassig? "has phenotype" : (row.minfit !== "" ? "has data" : "no data"));
     if (row.minfit !== "") {
 	sigstring = '<A TITLE="fitness ' + (+row.minfit).toFixed(1) + " to " + (+row.maxfit).toFixed(1)
 	    + '" HREF="' + fitblast_fitness_url(server_root,row.orgId,row.locusId) + '">'
@@ -74,15 +72,14 @@ function fitblast_load_short(id, server_root, sequence) {
     var URL = server_root + "cgi-bin/seqservice.cgi?seq=" + sequence;
     d3.tsv(URL, function(error, data) {
         if (error || data.length == 0) {
-            d.innerHTML = 'Cannot contact <A HREF="' + URL + '">server</A>';
+            d.innerHTML = 'Cannot contact <A HREF="' + URL + '">server</A> data.length = ' + data.length;
 	    return;
         }
 	//else
 	if ("Error" in data[0]) {
-	    d.innerHTML = data[0].Error;
+	    d.innerHTML = data[0].Error; // either "No hits" or an actual error
 	    return;
         }
-	//else
 	var closeRow = null; 	// closest hit with data, if above threshold
 	var pheRow = null;	// closest useful hit
 	var i;
@@ -90,7 +87,6 @@ function fitblast_load_short(id, server_root, sequence) {
 	    row = data[i];
 	    if (row.coverage < mincoverage) { continue; }
 	    var useful = fitblast_usefulhit(row);
-	    //console.log(row.organism + " " + (useful?"useful":"not"));
 	    if (!closeRow && row.identity >= mincloseid && row.minfit !== "") {
 		closeRow = row;
 		if (useful) { break; }
@@ -102,6 +98,7 @@ function fitblast_load_short(id, server_root, sequence) {
 	var out = [];
 	if (closeRow) { out.push(fitblast_short(closeRow,server_root)); }
 	if (pheRow) { out.push(fitblast_short(pheRow,server_root)); }
+        if (!closeRow && !pheRow) { out.push("No hits"); }
 	d.innerHTML = out.join("<BR>");
     });
 }
@@ -135,27 +132,26 @@ function fitblast_load_table(id, server_root, sequence) {
 	    .enter().append("th").text(function(column) { return column; });
 	var rows = tbody.selectAll("tr").data(data).enter().append("tr");
 	var cells = rows.selectAll("td").data(function(row) {
-		//console.log(Object.keys(row).join(" "));
 		var showCofit = "";
 		if (row.maxcofit >= mincofit) {
 		    showCofit = '<A HREF="' + fitblast_cofit_url(server_root, row.orgId, row.locusId) + '">'
 			+ (+row.maxcofit).toFixed(2) + '</A>';
 		}		var showFit = "No data";
 		if (row.minfit !== "") {
-		    var colLo = row.minfit < -2 ? "#0000FF" : (row.minfit < -1 ? "#7777FF" : "#777777");
-		    var colHi = row.maxfit > 2 ? "#0000FF" : (row.maxfit > 1 ? "#7777FF" : "#777777");
-		    showFit = '<small><div style="color:' + colLo + '">' + (+row.minfit).toFixed(1)
-			+ '</div> <div style="color:' + colHi + '">' + (+row.maxfit).toFixed(1)
-			+ '</div></small>';
+		    var colLo = row.minfit < -2 ? "#0000FF" : (row.minfit < -1 ? "#7777FF" : "#666666");
+		    var colHi = row.maxfit > 2 ? "#0000FF" : (row.maxfit > 1 ? "#AAAA00" : "#666666");
+		    showFit = '<span style="color:' + colLo + '">' + (+row.minfit).toFixed(1) + '</span>'
+                        + '<span style="color:#666666"> <small>to</small> </span>'
+                        + '<span style="color:' + colHi + '">' + (+row.maxfit).toFixed(1) + '</span>';
 		    if (fitblast_usefulhit(row)) { showFit = "<b>" + showFit + "</b>"; }
 		}
 		var show = { Organism: row.organism,
 			     Locus: fitblast_locus_with_link(row, server_root),
-			     Identity: '<small>' + row.identity + "%</small>",
+			     Identity: row.identity + "%",
 			     Description: "<small>" + row.description + "</small>",
 			     Coverage: (+row.coverage).toFixed(2),
 			     Fitness: showFit,
-			     Cofit: showCofit };
+			     Cofit: '&nbsp; ' + showCofit };
 		return columns.map(function(column) { return { column: column, value: show[column] } });
 	    }).enter().append("td").html(function(d) { return d.value; });
 	});
