9d153c129e0c79886ec2aecfce286a3bd541fa2c
Morgan Price <funwithwords26@gmail.com>
2016-10-31 14:53:06 -0700

    Die if no sequence or empty sequence for a scaffold

--- bin/gbkToSeq.pl
+++ bin/gbkToSeq.pl
@@ -10,6 +10,12 @@ use Bio::SeqIO;
     my $obj = Bio::SeqIO->new(-file => $file, -format => "genbank");
     my $out = Bio::SeqIO->new(-format => "fasta");
     while(my $scaffold = $obj->next_seq) {
+        my $seq = $scaffold->seq();
+        die "No sequence for " . $scaffold->display_id() . " try gbkToSeq2.pl\n"
+            unless defined $seq;
+        die "Empty sequence for " . $scaffold->display_id() . " try gbkToSeq2.pl\n"
+            unless $seq =~ m/[acgtACGT]/;
+
 	my $sc2 = Bio::Seq->new(-display_id => $scaffold->display_id(), -seq => $scaffold->seq());
 	$out->write_seq($sc2); # just the raw identifier (nothing else)
     }
