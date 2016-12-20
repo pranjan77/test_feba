4bcbc8b8958618e80f5b287bba574350b8386e3e
Morgan Price <funwithwords26@gmail.com>
2016-12-19 20:49:42 -0800

    For compatability with newer blat (blat 35 or 36), runs large genes or regions without -fastMap. Also added filtering for short or low-identity hits.

--- bin/Essentiality.pl
+++ bin/Essentiality.pl
@@ -3,16 +3,30 @@
 use strict;
 use Getopt::Long;
 use FindBin qw($Bin);
+use File::Which;
 use lib "$Bin/../lib";
 use FindGene; # for LocationToGene()
 use FEBA_Utils; # for ReadTable(), ReadColumnNames(), ReadFasta()
 
 my $minReads = 2; # minimum #reads at each location to consider reliable
 my $minRegion = 100; # minimum size of region with no insertions to consider
+my $maxFastMapNt = 5*1000; # maximum size of query for BLAT in fastMap mode
+# The BLAT command line arguments do not necessarily prevent alignments below these
+# thresholds from being returned. So, these filters are implemented in perl.
+my $minLength = 50; # minimum length of alignment to assign a duplicate region
+my $minIdentity = 95; # minimum %identity to assign a duplicate region
+
+my $blatDef = "$Bin/blat";
+if (! -x $blatDef) {
+    $blatDef = which('blat');
+}
+my $blatShow = $blatDef ne "" ? "[ -blat $blatDef ]": "-blat blat_executable_path";
 
 my $usage = <<END
 Usage: Essentiality.pl [ -minReads $minReads ]
 	-out prefix -genome fasta_file -genes tab_file mapped1.tab ... mappedN.tab
+	$blatShow
+        [ -minLength $minLength ] [ -minIdentity $minIdentity ]
 
 The mapped.tab files are from MapTnSeq.pl. The genes table must include the fields
     locusId, scaffoldId, begin, end, strand
@@ -26,12 +40,23 @@ END
     my $out = undef;
     my $genomeFile = undef;
     my $genesFile = undef;
+    my $blat = $blatDef;
     die $usage unless GetOptions('minReads=i' => \$minReads,
 				 'out=s' => \$out,
 				 'genome=s' => \$genomeFile,
-				 'genes=s' => \$genesFile)
+				 'genes=s' => \$genesFile,
+                                 'blat=s' => \$blat,
+                                 'minLength=i' => \$minLength,
+                                 'minIdentity=f' => \$minIdentity )
 	&& defined $out && defined $genomeFile && defined $genesFile
 	&& @ARGV > 0;
+    die "No blat executable specified\n" unless $blat;
+    die "No such executable: $blat\n" unless -x $blat;
+    die "Invalid value for -minLength: over 100 nt\n" if $minLength > 100;
+    die "Invalid value for -minIdentity: must be under 100%\n" if $minLength >= 100;
+    print STDERR "BLAT version:\n";
+    system("$blat | head -n 1");
+    
     my @mapfiles = @ARGV;
     my $seqs = &ReadFasta($genomeFile); # scaffoldId => nucleotide sequence
     my $totlen = 0;
@@ -76,12 +101,6 @@ END
 	}
     }
 
-    my $blat = "$Bin/blat";
-    if (! -x $blat) {
-        $blat = which("blat");
-	die "blat is not in $Bin or on path" if ! defined $blat;
-    }
-
     my $nReads = 0;
     my $nSkip = 0; # high qBeg
     my $nUsed = 0; # (genomic and OK qBeg only)
@@ -101,14 +120,14 @@ END
 		$nSkip++;
 		next;
 	    }
-	    # accept possibly non-unique reads ss the bias is to avoid false positives
+	    # accept possibly non-unique reads as the bias is to avoid false positives
 	    $counts{$scaffoldId}{$strand}{$pos}++;
 	    $nUsed++;
 	}
 	close(MAP) || die "Error reading $mapfile";
     }
 
-    # Count the "good" reads and write tehm out along with what gene they are in (if any)
+    # Count the "good" reads and write them out along with what gene they are in (if any)
     # This does not address the non-unique genome issue
     open(POS, ">", "$out.pos") || die "Cannot write to $out.pos";
     print POS join("\t", qw{scaffoldId strand pos nReads locusId f})."\n";
@@ -141,37 +160,64 @@ END
     print STDERR "Wrote $out.pos\n";
 
     # And write out reads per gene
+    # The original code was for BLAT v. 32x1, which allows -fastMap with large queries
+    # However as of BLAT 35, -fastMap does not work with queries of over 5K
     my $tmpFna = "$out.$$.fna";
+    my $tmpLongFna = "$out.$$.long.fna";
     my $tmpPsl = "$out.$$.psl";
+    my $tmpLongPsl = "$out.$$.long.psl";
+    my $nLong = 0;
     open(FNA, ">", $tmpFna) || die "Cannot write to $tmpFna";
+    open(FNA2, ">", $tmpLongFna) || die "Cannot write to $tmpLongFna";
     while (my ($scaffoldId, $genes) = each %genesSorted) {
 	my $scseq = $seqs->{$scaffoldId};
 	foreach my $gene (@$genes) {
 	    next unless defined $gene->{begin2} && $gene->{begin2} < $gene->{end2};
 	    # the non-overlapping part only; ignore strandedness
 	    my $ntseq = substr($scseq, $gene->{begin2}-1, $gene->{end2} - $gene->{begin2} + 1);
-	    print FNA ">".$gene->{locusId}."\n".$ntseq."\n";
+	    if (length($ntseq) >= $maxFastMapNt) {
+		print FNA2 ">".$gene->{locusId}."\n".$ntseq."\n";
+                $nLong++;
+	    }
+	    else {
+		print FNA ">".$gene->{locusId}."\n".$ntseq."\n";
+	    }
 	}
     }
     close(FNA) || die "Error writing to $tmpFna";
-    # ntoe -fastMap means no introns high %identity
+    close(FNA2) || die "Error writing to $tmpLongFna";
+    # note -fastMap means no introns high %identity
     my $blatcmd = "$blat -t=dna -out=blast8 -fastMap -minScore=50 $genomeFile $tmpFna $tmpPsl";
     system($blatcmd) == 0 || die "Error running $blatcmd\n$!";
+    if ($nLong > 0) {
+        my $blatcmd2 = "$blat -t=dna -out=blast8 -minScore=50 $genomeFile $tmpLongFna $tmpLongPsl";
+        system($blatcmd2) == 0 || die "Error running $blatcmd2\n$!";
+    } else {
+        # create empty tmpLongPsl if no long queries -- blat will fail with empty input
+        open(EMPTY, ">", $tmpLongPsl) || die "Cannot write to $tmpLongPsl";
+        close(EMPTY) || die "Error writing to $tmpLongPsl";
+    }
 
-    open(PSL, "<", $tmpPsl) || die "Error reading $tmpPsl";
+    open(PSL, "-|", "cat $tmpPsl $tmpLongPsl") || die "Error reading $tmpPsl $tmpLongPsl";
+    my $nPSL = 0;
     while(<PSL>) {
 	chomp;
 	my @F = split /\t/, $_, -1;
 	my ($query, $subject, $identity, $len, $mm, $gaps, $qBeg, $qEnd, $sBeg, $sEnd, $evalue, $score) = @F;
+        next unless $len >= $minLength && $identity >= $minIdentity;
 	my $gene = $locusToGene{$query};
 	die "Invalid locusId $query in BLAT results" unless defined $gene;
 	# ignore self hits
 	next if $subject eq $gene->{scaffoldId} && $sBeg >= $gene->{begin2} && $sEnd <= $gene->{end2};
 	$gene->{dupScore} = $score unless exists $gene->{dupScore} && $gene->{dupScore} > $score;
+        $nPSL++;
     }
-    close(PSL) || die "Error reading $tmpPsl";
+    print STDERR "Read $nPSL non-self hits from BLAT of genes\n";
+    close(PSL) || die "Error reading $tmpPsl $tmpLongPsl";
     unlink($tmpFna);
+    unlink($tmpLongFna);
     unlink($tmpPsl);
+    unlink($tmpLongPsl);
 
     my @geneHeader = ReadColumnNames($genesFile);
     push @geneHeader, qw{ntLenNoOverlap dupScore nPos nReads nPosCentral nReadsCentral};
@@ -194,6 +240,9 @@ END
     # Identify regions with no reads, and test if they are unique
     my %regions = (); # scaffoldId => begin => list of end, dupScore, GC
     open(FNA, ">", $tmpFna) || die "Cannot write to $tmpFna";
+    open(FNA2, ">", $tmpLongFna) || die "Cannot write to $tmpLongFna";
+    $nLong = 0;
+    my $nRegions = 0;
     foreach my $scaffoldId (keys %counts) {
 	my %insAt = ();
 	foreach my $strand (qw{+ -}) {
@@ -205,6 +254,7 @@ END
 	    my $beg = $insAt[$i-1] + 1;
 	    my $end = $insAt[$i] - 1;
 	    if ($end - $beg + 1 >= 100) {
+                $nRegions++;
 		my $seq = substr($seqs->{$scaffoldId}, $beg-1, $end-$beg+1);
 		my @hits;
 		@hits = $seq =~ m/[GC]/g;
@@ -213,26 +263,48 @@ END
 		my $nAT = scalar(@hits);
 		my $GC = $nGC/($nGC + $nAT);
 		$regions{$scaffoldId}{$beg} = [ $end, 0, $GC ];
-		print FNA ">$scaffoldId:$beg:$end\n$seq\n";
+		if (length($seq) >= $maxFastMapNt) {
+		    print FNA2 ">$scaffoldId:$beg:$end\n$seq\n";
+                    $nLong++;
+		}
+		else {
+		    print FNA ">$scaffoldId:$beg:$end\n$seq\n";
+		}
+
 	    }
 	}
     }
     close(FNA) || die "Error writing to $tmpFna";
    
     system($blatcmd) == 0 || die "Error running $blatcmd\n$!";
-    open(PSL, "<", $tmpPsl) || die "Cannot read $tmpPsl";
+    if ($nLong > 0) {
+        my $blatcmd2 = "$blat -t=dna -out=blast8 -minScore=50 $genomeFile $tmpLongFna $tmpLongPsl";
+        system($blatcmd2) == 0 || die "Error running $blatcmd2\n$!";
+    } else {
+        # create empty tmpLongPsl if no long queries -- blat will fail with empty input
+        open(EMPTY, ">", $tmpLongPsl) || die "Cannot write to $tmpLongPsl";
+        close(EMPTY) || die "Error writing to $tmpLongPsl";
+    }
+
+    open(PSL, "-|", "cat $tmpPsl $tmpLongPsl") || die "Cannot read $tmpPsl $tmpLongPsl";
+    $nPSL = 0;
     while(<PSL>) {
 	chomp;
 	my @F = split /\t/, $_, -1;
 	my ($query, $subject, $identity, $len, $mm, $gaps, $qBeg, $qEnd, $sBeg, $sEnd, $evalue, $score) = @F;
+        next unless $len >= $minLength && $identity >= $minIdentity;
 	my ($scaffoldId,$begin,$end) = split /:/, $query;
 	die "Invalid query $query" unless defined $end && $regions{$scaffoldId}{$begin}[0] == $end;
 	next if $subject eq $scaffoldId && $sBeg == $begin && $sEnd == $end; # ignore hit to itself
+        $nPSL++;
 	$regions{$scaffoldId}{$begin}[1] = $score unless $regions{$scaffoldId}{$begin}[1] > $score;
     }
-    close(PSL) || die "Error reading $tmpPsl";
+    close(PSL) || die "Error reading $tmpPsl $tmpLongPsl";
+    print STDERR "Read $nPSL non-self hits from BLAT of $nRegions regions\n";
     unlink($tmpFna);
+    unlink($tmpLongFna);
     unlink($tmpPsl);
+    unlink($tmpLongPsl);
 
     open(REGIONS, ">", "$out.regions") || die "Cannot write to $out.regions";
     print REGIONS join("\t", qw{scaffoldId begin end GC dupScore})."\n";
